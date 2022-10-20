8a9ebdde9840bc46f3796a3d3c57459af9e7ed68
wl99-human <weileelim2012@gmail.com>
2022-10-20 23:48:47 +0800

    Initial commit

---
+++ Tutorials/01-helloworld/myhelloworld.ts
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("Coding Gurus! " + "Time for some fun!");
+
+let myData: any = 50.0
+
+// Can assign different values of any type
+myData = false;
+myData = 'Eric';
+myData = 19;
+
---
+++ Tutorials/sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Lim";
+let lastName: string = "Wei Lee";
+
+console.log(found);
+console.log("The grade is " + grade);
+
+console.log("Hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
