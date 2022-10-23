c3a2c066bfbdce9190650b2d9df1d5ce540eef48
d1349598 <weilee.weilee@ncs.com.sg>
2022-10-23 21:23:27 +0800

    06-abstract-classes

---
+++ TYPESCRIPT-TRAINING/06-abstract-classes/Circle.ts
@@ -0,0 +1,24 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape {
+    public calculateArea(): number {
+        return Math.PI * Math.pow(this._radius, 2);
+    }
+
+    constructor(theX: number, theY: number, 
+        private _radius: number) {
+            super(theX, theY);
+        }
+
+    public get radius(): number {
+        return this._radius;
+    }
+
+    public set radius(value: number) {
+        this._radius = value;
+    }
+    
+    public getInfo(): string {
+        return super.getInfo() + `, radius=${this._radius}`;
+    }
+}
