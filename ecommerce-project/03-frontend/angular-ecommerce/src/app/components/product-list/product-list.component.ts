150f03e3e5ea6d73d510f0413bb5b37391b8aa7c
wl99-human <weileelim2012@gmail.com>
2022-10-30 23:58:22 +0800

    eCommerce Project - Search for Products by Keyword

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
   currentCategoryName: string = "";
+  searchMode!: boolean;
   
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -24,7 +25,27 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
