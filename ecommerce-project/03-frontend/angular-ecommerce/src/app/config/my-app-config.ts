c3ee37e8ae5bac91dd56b6791d0dfa317eadd12c
wl99-human <weileelim2012@gmail.com>
2022-11-06 21:58:47 +0800

    Security - Login/Logout

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa755ttj5JLWdRoY5d7',
+        issuer: 'https://dev-00222799.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
