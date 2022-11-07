1f3debbf61fb6c49f7637143395dd095c46601b3
wl99-human <weileelim2012@gmail.com>
2022-11-07 21:52:45 +0800

    Modify cart WebStorage to session from local

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -15,8 +15,8 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  //storage: Storage = sessionStorage;
-  storage: Storage = localStorage;
+  storage: Storage = sessionStorage;
+  //storage: Storage = localStorage;
 
   constructor() {
     // read data from storage
