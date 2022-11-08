70ee8e66ab7c67590c2f53f9719bfc11f5a89935
wl99-human <weileelim2012@gmail.com>
2022-11-08 20:55:22 +0800

    Secure Communication with HTTPS - Angular Frontend

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa755ttj5JLWdRoY5d7',
         issuer: 'https://dev-00222799.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
