b282b130889ffbf5018225e904e0c6833f540c07
wl99-human <weileelim2012@gmail.com>
2022-11-09 16:46:30 +0800

    Angular Environment Configuration

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { from, lastValueFrom, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,8 @@ export class AuthInterceptorService implements HttpInterceptor{
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
     // Only add an access token for secured endpoints
-    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    const theEndPoint = environment.luv2shopApiUrl + '/orders';
+    const securedEndpoints = [theEndPoint];
 
     if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
       // get access token
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Country } from '../common/country';
 import { State } from '../common/state';
 
@@ -8,9 +9,8 @@ import { State } from '../common/state';
   providedIn: 'root'
 })
 export class Luv2ShopFormService {
-
-  private countriesUrl = 'http://localhost:8080/api/countries';
-  private statesUrl = 'http://localhost:8080/api/states';
+  private countriesUrl = environment.luv2shopApiUrl + '/countries';
+  private statesUrl = environment.luv2shopApiUrl + '/states';
 
   constructor(private httpClient: HttpClient) { }
 
